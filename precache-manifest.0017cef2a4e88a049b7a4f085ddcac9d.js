fd22488d7abec7b2ab8477be6605b8ab8b99a717
Gui.go <guilhermeviegas1993@gmail.com>
2020-07-21 11:23:23 -0300

    Updates

---
+++ precache-manifest.0017cef2a4e88a049b7a4f085ddcac9d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56bbd063e3643279981918b6b104f702",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b42ff0503121c5697c20",
+    "url": "/portfolio/static/css/2.3d60f3ae.chunk.css"
+  },
+  {
+    "revision": "32079448be952662d500",
+    "url": "/portfolio/static/css/main.c928c60a.chunk.css"
+  },
+  {
+    "revision": "b42ff0503121c5697c20",
+    "url": "/portfolio/static/js/2.877f3209.chunk.js"
+  },
+  {
+    "revision": "c1ed50e0a7b9aba257ce7f51574ada94",
+    "url": "/portfolio/static/js/2.877f3209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32079448be952662d500",
+    "url": "/portfolio/static/js/main.fbfad189.chunk.js"
+  },
+  {
+    "revision": "9f21c293b6cce5333498",
+    "url": "/portfolio/static/js/runtime-main.daff2ce6.js"
+  },
+  {
+    "revision": "c8835fdf5fb0210ac4ad401939013a35",
+    "url": "/portfolio/static/media/art1.c8835fdf.jpg"
+  },
+  {
+    "revision": "3ac38022270cba535eeeb1fb87089502",
+    "url": "/portfolio/static/media/art2.3ac38022.jpg"
+  },
+  {
+    "revision": "b023b140d0e38fb779c1928f0662b77d",
+    "url": "/portfolio/static/media/cloud-cover.b023b140.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "bbe42186f0cbbc43d2f115f249b8e8a4",
+    "url": "/portfolio/static/media/guigo.bbe42186.jpg"
+  }
+]);
