bba6a56b456f3aa40e2cee4d806f97c0d8d876ac
viegas <guilherme.viegas@aquare.la>
2020-12-22 01:07:01 -0300

    update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6a8252375d288edead548d1735cdb8bf.js"
+  "/portfolio/precache-manifest.0017cef2a4e88a049b7a4f085ddcac9d.js"
 );
 
 self.addEventListener('message', (event) => {
